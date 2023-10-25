5a0b4faf3cfc46979e00ddd9ebeceba6d17b1a90
Alvin Alfandy <64345368+alvinalfandy@users.noreply.github.com>
2023-10-25 12:36:48 +0700

    Add files via upload

---
+++ latihan.py
@@ -0,0 +1,14 @@
+jarijari = float(input("Masukkan jari-jari lingkaran: ")) 
+# kode meminta pengguna untuk menginputkan jari jari
+
+luaslingkaran = 3.14 * jarijari**2  
+# kode rumus luas lingkaran
+
+kelilinglingkaran = 2 * 3.14 * jarijari 
+# kode rumus keliling lingkaran
+
+print("Luas lingkaran adalah: ", luaslingkaran)
+# kode untuk menampilkan hasil luas lingkaran
+
+print("Keliling lingkaran adalah: ", kelilinglingkaran)
+# kode untuk menampilkan hasil keliling lingkaran
